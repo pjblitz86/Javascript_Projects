c7b8dc863b08a747835c3cd7bda0dc9fe67cb387
Pavelas Adomaitis <paulius.juknis@gmail.com>
2018-01-31 09:41:47 +0200

    commit1

---
+++ JS_Sandbox/app8.js
@@ -0,0 +1,22 @@
+// DATE AND TIME
+
+let val;
+
+const today = new Date();
+let birthday = new Date('9-10-1981 11:25:00'); // various ways, see documentation
+birthday = new Date('September 10 1981');
+birthday = new Date('9/10/1981');
+
+val = today.getMonth(); // january is 0
+val = today.getDate(); // day of month
+val = today.getDay(); // week day; sunday is 0
+val = today.getFullYear();
+val = today.getHours();
+val = today.getMinutes();
+val = today.getSeconds();
+
+val = today.getTime(); // time stamp since 1970 in seconds
+
+birthday
+
+console.log(val);
