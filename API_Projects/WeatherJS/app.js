5cfc8517679943cc1d2fdf9b12d828ecbbab170e
Paulius Juknis <paulius.juknis@gmail.com>
2018-06-26 09:47:54 +0300

    weatherJS - persist data to local storage

--- API_Projects/WeatherJS/app.js
+++ API_Projects/WeatherJS/app.js
@@ -1,12 +1,30 @@
+// Init local storage
+const storage = new Storage();
+// Get stored location data
+const weatherLocation = storage.getLocationData();
 // init weather object
-const weather = new Weather('Boston', 'MA');
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
 // init UI object
 const ui = new UI();
 
 // Get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather);
 
-// weather.changeLocation('Miami', 'FL');
+// Change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+  const city = document.getElementById('city').value;
+  const state = document.getElementById('state').value;
+  // Change location
+  weather.changeLocation(city, state);
+  // Set location in local storage
+  storage.setLocationData(city, state)
+  // get and display weather 
+  getWeather();
+  // Close modal
+  $('#locModal').modal('hide');
+
+});
+
 
 function getWeather() {
   weather.getWeather()
