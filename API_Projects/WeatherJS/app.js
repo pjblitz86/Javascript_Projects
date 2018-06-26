331d44e63959e515103ccccc03b0cc967accf2ba
Paulius Juknis <paulius.juknis@gmail.com>
2018-06-26 09:27:36 +0300

    weatherJS - display the weather in the UI

--- API_Projects/WeatherJS/app.js
+++ API_Projects/WeatherJS/app.js
@@ -1,5 +1,7 @@
 // init weather object
 const weather = new Weather('Boston', 'MA');
+// init UI object
+const ui = new UI();
 
 // Get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather);
@@ -9,7 +11,7 @@ document.addEventListener('DOMContentLoaded', getWeather);
 function getWeather() {
   weather.getWeather()
     .then(results => {
-      console.log(results);
+      ui.paint(results);
     })
     .catch(err => console.log(err));
   }
