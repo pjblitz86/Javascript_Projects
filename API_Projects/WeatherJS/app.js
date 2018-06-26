da24cb1adee79a06a1d532f7b92f80dd653b1eee
Paulius Juknis <paulius.juknis@gmail.com>
2018-06-26 09:10:22 +0300

    weatherJS - fetch weather data from api and test via console.log

---
+++ API_Projects/WeatherJS/app.js
@@ -0,0 +1,15 @@
+// init weather object
+const weather = new Weather('Boston', 'MA');
+
+// Get weather on DOM load
+document.addEventListener('DOMContentLoaded', getWeather);
+
+// weather.changeLocation('Miami', 'FL');
+
+function getWeather() {
+  weather.getWeather()
+    .then(results => {
+      console.log(results);
+    })
+    .catch(err => console.log(err));
+  }
