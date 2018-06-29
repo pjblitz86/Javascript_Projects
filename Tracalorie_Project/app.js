9b80c11c5e5eb80d7472ad46aa7197eab6256c79
Paulius Juknis <paulius.juknis@gmail.com>
2018-06-29 09:43:00 +0300

    added controllers for module pattern

--- Tracalorie_Project/app.js
+++ Tracalorie_Project/app.js
@@ -1,7 +1,52 @@
-// Storage Controller
+// STORAGE Controller
 
-// Item Controller
+// ITEM Controller
+const ItemCtrl = (function() {
+  // Item constructor
+  const Item = function(id, name, calories) {
+    this.id = id;
+    this.name = name;
+    this.calories = calories;
+  }
+
+  // Data structure / STATE
+  const data = {
+    items: [
+      {id: 0, name: 'Steak Dinner', calories: 1200},
+      {id: 1, name: 'Cookie', calories: 400},
+      {id: 2, name: 'Eggs', calories: 300}
+    ],
+    currentItem: null,
+    totalCalories: 0
+  }
+
+  // Public methods in return
+  return {
+    logData: function() {
+      return data;
+    }
+  }
+})();
+
+
 
 // UI Controller
+const UICtrl = (function() {
+  
+})();
 
-// App Controller
+
+
+// APP Controller - MAIN - all modules meet here 
+const App = (function(ItemCtrl, UICtrl) {
+  
+  // Public methods in return
+  return {
+    init: function() {
+      console.log('Initializing app...');
+    }
+  }
+})(ItemCtrl, UICtrl);
+
+// Initialize app
+App.init();
