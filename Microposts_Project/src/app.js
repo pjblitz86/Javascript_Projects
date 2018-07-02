1dba210950fc139958dd564c9f233525d70d7c0f
Paulius Juknis <paulius.juknis@gmail.com>
2018-07-02 11:48:40 +0300

    post edit state and update

--- Microposts_Project/src/app.js
+++ Microposts_Project/src/app.js
@@ -10,6 +10,12 @@ document.querySelector('.post-submit').addEventListener('click', submitPost);
 // Listen for delete
 document.querySelector('#posts').addEventListener('click', deletePost);
 
+// Listen for edit state
+document.querySelector('#posts').addEventListener('click', enableEdit);
+
+// Listen for cancel
+document.querySelector('.card-form').addEventListener('click', cancelEdit);
+
 // Get Posts
 function getPosts() {
   http.get('http://localhost:3000/posts')
@@ -21,20 +27,37 @@ function getPosts() {
 function submitPost() {
   const title = document.querySelector('#title').value;
   const body = document.querySelector('#body').value;
-
+  const id = document.querySelector('#id').value;
+  
   const data = {
     title,
     body
   }
+  // validate input
+  if(title === '' || body === '') {
+    ui.showAlert('Please fill in all fields', 'alert alert-danger')
+  } else {
 
-  // Create Post
-  http.post('http://localhost:3000/posts', data)
-    .then(data => {
-      ui.showAlert('Post added', 'alert alert-success');
-      ui.clearFields();
-      getPosts();
-    })
-    .catch(err => console.log(err));
+    if(id === '') {
+      // Create Post
+      http.post('http://localhost:3000/posts', data)
+      .then(data => {
+        ui.showAlert('Post added', 'alert alert-success');
+        ui.clearFields();
+        getPosts();
+      })
+      .catch(err => console.log(err));
+    } else {
+      // Update the post
+      http.put(`http://localhost:3000/${id}`, data)
+      .then(data => {
+        ui.showAlert('Post updated', 'alert alert-success');
+        ui.changeFormState('add');
+        getPosts();
+      })
+      .catch(err => console.log(err));
+    }
+  }
 }
 
 // Delete Post
@@ -51,4 +74,33 @@ function deletePost(e) {
          }
      }
      e.preventDefault();
- }
+}
+
+// Enable edit state
+function enableEdit(e) {
+  if(e.target.parentElement.classList.contains('edit')) {
+    const id = e.target.parentElement.dataset.id;
+    const body = e.target.parentElement.previousElementSibling.textContent;
+    const title = e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
+    
+    const data = {
+      id,
+      title,
+      body
+    }
+
+    // Fill for with the current post
+    ui.fillForm(data);
+  }  
+
+   e.preventDefault();
+}
+
+// Cancel Edit State
+function cancelEdit(e) {
+  if(e.target.classList.contains('post-cancel')) {
+    ui.changeFormState('add');
+  }
+
+  e.preventDefault();
+}
--- Microposts_Project/src/ui.js
+++ Microposts_Project/src/ui.js
@@ -60,9 +60,55 @@ class UI {
     }
   }
 
+  // clear all fields
   clearFields() {
     this.titleInput.value = '';
     this.bodyInput.value = '';
+  }
+
+  // Fill form to edit
+  fillForm(data) {
+    this.titleInput.value = data.title;
+    this.bodyInput.value = data.body;
+    this.idInput.value = data.id;
+
+    this.changeFormState('edit');
+  }
+
+  // clear id input
+  clearIdInput() {
+    this.idInput.value = '';
+  }
+
+  // change the form state
+  changeFormState(type) {
+    if(type === 'edit') {
+      this.postSubmit.textContent = 'Update Post';
+      this.postSubmit.className = 'post-submit btn btn-warning btn-block';
+
+      // create cancel button
+      const button = document.createElement('button');
+      button.className = 'post-cancel btn btn-light btn-block';
+      button.appendChild(document.createTextNode('Cancel Edit'));
+
+      // Get parent
+      const cardForm = document.querySelector('.card-form');
+      // get element to insert before
+      const formEnd = document.querySelector('.form-end');
+      // insert cancel button
+      cardForm.insertBefore(button, formEnd);
+    } else {
+      this.postSubmit.textContent = 'Post It';
+      this.postSubmit.className = 'post-submit btn btn-primary btn-block';
+      // remove cancel button if it's there
+      if(document.querySelector('.post-cancel')) {
+        document.querySelector('.post-cancel').remove();
+      }
+      // Clear ID from hidden field
+      this.clearIdInput();
+      // clear the text fields
+      this.clearFields();
+    }
   }
 }
 
