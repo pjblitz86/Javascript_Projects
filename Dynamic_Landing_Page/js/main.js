4b4a9df0f5300803e647b502be3940b9e1e9583b
Paulius Juknis <paulius.juknis@gmail.com>
2019-06-11 14:58:51 +0300

    showTime function implementation

---
+++ Dynamic_Landing_Page/js/main.js
@@ -0,0 +1,22 @@
+const time = document.getElementById('time'),
+  greeting = document.getElementById('greeting'),
+  name = document.getElementById('name'),
+  focus = document.getElementById('focus');
+
+function showTime() {
+  let today = new Date(),
+    hour = today.getHours(),
+    min = today.getMinutes(),
+    sec = today.getSeconds();
+
+  const AMorPM = hour >= 12
+    ? 'PM'
+    : 'AM';
+
+  hour = hour % 12 || 12;
+
+  time.innerHTML = `${hour}<span>:</span>${min}<span>:</span>${sec} ${AMorPM}`;
+  setTimeout(showTime, 1000);
+}
+
+showTime();
