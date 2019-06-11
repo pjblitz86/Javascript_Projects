868602e399c68046f693bf36236d923b5a3d03af
Paulius Juknis <paulius.juknis@gmail.com>
2019-06-11 16:07:12 +0300

    setName and setFocus implemented

--- Dynamic_Landing_Page/js/localStorage.js
+++ Dynamic_Landing_Page/js/localStorage.js
@@ -9,13 +9,40 @@ function getName() {
   }
 }
 
+function setName(e) {
+  if (e.type === 'keypress') {
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem('name', e.target.innerText);
+      name.blur();
+    }
+  } else {
+    localStorage.setItem('name', e.target.innerText);
+  }
+}
+
 function getFocus() {
   if (localStorage.getItem('focus') === null) {
     focus.textContent = '[Enter Focus]';
   } else {
-    name.textContent = localStorage.getItem('focus');
+    focus.textContent = localStorage.getItem('focus');
   }
 }
+
+function setFocus(e) {
+  if (e.type === 'keypress') {
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem('focus', e.target.innerText);
+      focus.blur();
+    }
+  } else {
+    localStorage.setItem('focus', e.target.innerText);
+  }
+}
+
+name.addEventListener('keypress', setName);
+name.addEventListener('blur', setName);
+focus.addEventListener('keypress', setFocus);
+focus.addEventListener('blur', setFocus);
 
 getName();
 getFocus();
