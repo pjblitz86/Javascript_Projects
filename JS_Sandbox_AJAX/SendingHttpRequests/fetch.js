34e16165923beb2ab95469cbb1ab75c57a9e8f05
Paulius_Juknis <paulius.juknis@gmail.com>
2019-10-03 16:40:59 +0300

    sending http requests via fetch api

--- JS_Sandbox_AJAX/SendingHttpRequests/fetch.js
+++ JS_Sandbox_AJAX/SendingHttpRequests/fetch.js
@@ -1,9 +1,39 @@
 const getBtn = document.getElementById("get-btn");
 const postBtn = document.getElementById("post-btn");
 
-const getData = () => {};
+const sendHttpRequest = (method, url, data) => {
+  return fetch(url, {
+    method: method,
+    body: JSON.stringify(data),
+    headers: data ? { "Content-Type": "application/json" } : {}
+  }).then(response => {
+    if (response.status >= 400) {
+      return response.json().then(errResData => {
+        const error = new Error("sum ding wong");
+        error.data = errResData;
+        throw error;
+      });
+    }
+    return response.json();
+  });
+};
 
-const sendData = () => {};
+const getData = () => {
+  sendHttpRequest("GET", "https://reqres.in/api/users").then(responseData => {
+    console.log(responseData);
+  });
+};
+
+const sendData = () => {
+  sendHttpRequest("POST", "https://reqres.in/api/register", {
+    email: "eve.holt@reqres.in",
+    password: "pistol"
+  })
+    .then(responseData => {
+      console.log(responseData);
+    })
+    .catch(err => console.log(err));
+};
 
 getBtn.addEventListener("click", getData);
 postBtn.addEventListener("click", sendData);
