857bfcb1063aa340e4e531772eafc9362a82752a
Paulius_Juknis <paulius.juknis@gmail.com>
2019-11-15 11:29:50 +0200

    add array and object

--- JS_Sandbox/Basics/assets/scripts/app.js
+++ JS_Sandbox/Basics/assets/scripts/app.js
@@ -1,5 +1,6 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
 function getUserInput() {
   return parseInt(userInput.value);
@@ -10,11 +11,23 @@ function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
   outputResult(currentResult, calcDescription);
 }
 
+function log(operation, initialResult, enteredNumber, currentResult) {
+  const logEntry = {
+    operation,
+    prevResult: initialResult,
+    number: enteredNumber,
+    result: currentResult
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
+
 function add() {
   const enteredNumber = getUserInput();
   const initialResult = currentResult;
   currentResult += enteredNumber;
   createAndWriteOutput("+", initialResult, enteredNumber);
+  log("add", initialResult, enteredNumber, currentResult);
 }
 
 function subtract() {
@@ -22,6 +35,7 @@ function subtract() {
   const initialResult = currentResult;
   currentResult -= enteredNumber;
   createAndWriteOutput("-", initialResult, enteredNumber);
+  log("subtract", initialResult, enteredNumber, currentResult);
 }
 
 function multiply() {
@@ -29,6 +43,7 @@ function multiply() {
   const initialResult = currentResult;
   currentResult *= enteredNumber;
   createAndWriteOutput("*", initialResult, enteredNumber);
+  log("multiply", initialResult, enteredNumber, currentResult);
 }
 
 function divide() {
@@ -36,6 +51,7 @@ function divide() {
   const initialResult = currentResult;
   currentResult /= enteredNumber;
   createAndWriteOutput("/", initialResult, enteredNumber);
+  log("divide", initialResult, enteredNumber, currentResult);
 }
 
 addBtn.addEventListener("click", add);
