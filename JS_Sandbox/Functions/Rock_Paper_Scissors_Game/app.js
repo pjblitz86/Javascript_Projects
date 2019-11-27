3789ca7191647fc1682626a9ccbcb60234784076
Paulius_Juknis <paulius.juknis@gmail.com>
2019-11-27 09:03:04 +0200

    core game logic

--- JS_Sandbox/Functions/Rock_Paper_Scissors_Game/app.js
+++ JS_Sandbox/Functions/Rock_Paper_Scissors_Game/app.js
@@ -4,6 +4,9 @@ const ROCK = "ROCK";
 const PAPER = "PAPER";
 const SCISSORS = "SCISSORS";
 const DEFAULT_CHOICE = ROCK;
+const RESULT_DRAW = "DRAW";
+const RESULT_PLAYER_WINS = "PLAYER WINS";
+const RESULT_COMPUTER_WINS = "COMPUTER WINS";
 let gameIsRunning = false;
 
 const getPlayerChoice = function() {
@@ -18,12 +21,39 @@ const getPlayerChoice = function() {
   return selection;
 };
 
+const getComputerChoice = function() {
+  const randomValue = Math.random();
+  if (randomValue < 0.34) {
+    return ROCK;
+  } else if (randomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
+
+const getWinner = function(cChoice, pChoice) {
+  if (cChoice === pChoice) {
+    return RESULT_DRAW;
+  } else if (
+    (cChoice === ROCK && pChoice === PAPER) ||
+    (cChoice === PAPER && pChoice === SCISSORS) ||
+    (cChoice === SCISSORS && pChoice === ROCK)
+  ) {
+    return RESULT_PLAYER_WINS;
+  } else {
+    return RESULT_COMPUTER_WINS;
+  }
+};
+
 startGameBtn.addEventListener("click", function() {
   if (gameIsRunning) {
     return;
   }
   gameIsRunning = true;
   console.log("game is starting...");
-  const playerSelection = getPlayerChoice();
-  console.log(playerSelection);
+  const playerChoice = getPlayerChoice();
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(computerChoice, playerChoice);
+  console.log(winner);
 });
