6424bf17b95009d27b633a950eaef894b21dcf63
Paulius_Juknis <paulius.juknis@gmail.com>
2019-11-28 10:23:30 +0200

    DOM assignment

---
+++ JS_DOM/Work_In_DOM/Assignment/assignment.js
@@ -0,0 +1,13 @@
+const task1 = document.getElementById("task-1");
+// const task1 = document.querySelector('#task-1');
+
+task1.style.backgroundColor = "black";
+task1.style.color = "white";
+
+// const documentTitle = document.querySelector("title");
+// documentTitle.textContent = "Assignment - Solved!";
+
+document.title = "Assignment - Solved!";
+
+const h1 = document.querySelector("h1");
+h1.textContent = "Assignment - Solved!";
