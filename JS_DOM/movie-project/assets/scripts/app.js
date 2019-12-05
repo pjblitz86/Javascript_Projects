80abe765122e305f1eca7adf77929ff026ea2754
Paulius_Juknis <paulius.juknis@gmail.com>
2019-12-05 14:24:54 +0200

    simple validation for inputs

--- JS_DOM/movie-project/assets/scripts/app.js
+++ JS_DOM/movie-project/assets/scripts/app.js
@@ -1,17 +1,45 @@
 const addMovieModal = document.getElementById("add-modal");
 const startAddMovieBtn = document.querySelector("header button");
 const backdrop = document.getElementById("backdrop");
-const modalCancelBtn = document.getElementsByClassName("btn--passive")[0];
+const cancelAddMovieBtn = addMovieModal.querySelector(".btn--passive");
+const confirmAddMovieBtn = cancelAddMovieBtn.nextElementSibling;
+const userInputs = addMovieModal.querySelectorAll("input");
 
 const toggleMovieModal = () => {
   addMovieModal.classList.toggle("visible");
-  toggleBackdrop();
+  toggleBackdropHandler();
 };
 
-const toggleBackdrop = () => {
+const toggleBackdropHandler = () => {
   backdrop.classList.toggle("visible");
 };
 
-startAddMovieBtn.addEventListener("click", toggleMovieModal, toggleBackdrop);
+const backdropClickHandler = () => {
+  toggleMovieModal();
+};
 
-modalCancelBtn.addEventListener("click", toggleMovieModal);
+const cancelAddMovieHandler = () => {
+  toggleMovieModal();
+};
+
+const addMovieHandler = () => {
+  const titleValue = userInputs[0].value;
+  const imageUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+
+  if (
+    titleValue.trim() === "" ||
+    imageUrlValue.trim() === "" ||
+    ratingValue.trim() === "" ||
+    ratingValue < 1 ||
+    ratingValue > 5
+  ) {
+    alert("Please enter valid values");
+    return;
+  }
+};
+
+startAddMovieBtn.addEventListener("click", toggleMovieModal);
+backdrop.addEventListener("click", backdropClickHandler);
+cancelAddMovieBtn.addEventListener("click", cancelAddMovieHandler);
+confirmAddMovieBtn.addEventListener("click", addMovieHandler);
