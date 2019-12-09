b9ff52c0d16c30ca2980902719b49a2835988e0d
Paulius_Juknis <paulius.juknis@gmail.com>
2019-12-09 15:32:11 +0200

    filter,split,join

--- JS_Sandbox/JS_Data_Structures/Array_Manipulation/app4.js
+++ JS_Sandbox/JS_Data_Structures/Array_Manipulation/app4.js
@@ -26,8 +26,31 @@ console.log(taxAdjustedPrices2);
 // SORT AND REVERSE
 
 prices2.sort((a, b) => {
-  return a - b;
+  return a - b; // works for numbers but not strings
 });
 
 prices2.reverse(); // or you could do it in sort return b-a
 console.log(prices2);
+
+// FILTER - will return new array and not touch old, up to 3 arguments
+const newPirces2 = prices2.filter(price => price >= 4); // arrow functions shine, this is short
+console.log(newPirces2);
+
+// REDUCE - tricky but very useful - reduce array to a simpler value in this case sum of prices
+const sum = prices2.reduce(
+  (accumulator, currentValue, currentIndex, prices2) =>
+    accumulator + currentValue,
+  0 // 0 is the starting value
+);
+
+console.log(sum);
+
+// SPLIT AND JOIN - for strings
+const cities = "york,kaunas,vilnius";
+const citiesArr = cities.split(","); // seperator by which you split
+
+console.log(citiesArr);
+
+const nameFragments = ["Paul", "Jukman"];
+const fullNameArr = nameFragments.join(" ");
+console.log(fullNameArr);
