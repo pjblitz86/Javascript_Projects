1c78e90c4081c14a89335dee467a021c3486bb02
Paulius_Juknis <paulius.juknis@gmail.com>
2019-12-11 08:48:22 +0200

    rendering new movie element

--- JS_Sandbox/JS_Data_Structures/Objects/assets/scripts/objects.js
+++ JS_Sandbox/JS_Data_Structures/Objects/assets/scripts/objects.js
@@ -3,13 +3,34 @@ const searchBtn = document.getElementById("search-btn");
 
 const movies = [];
 
+const renderMovies = () => {
+  const movieList = document.getElementById("movie-list");
+  if (movies.length === 0) {
+    movieList.classList.remove("visible");
+    return;
+  } else {
+    movieList.classList.add("visible");
+  }
+  movieList.innerHTML = "";
+
+  movies.forEach(movie => {
+    const movieEl = document.createElement("li");
+    movieEl.textContent = movie.info.title;
+    movieList.append(movieEl);
+  });
+};
+
 const addMovieHandler = () => {
   const title = document.getElementById("title").value;
   const extraName = document.getElementById("extra-name").value;
   const extraValue = document.getElementById("extra-value").value;
 
   // simple validation
-  if (title.trim() === "" || extraName.trim() === "" || extraValue.trim()) {
+  if (
+    title.trim() === "" ||
+    extraName.trim() === "" ||
+    extraValue.trim() === ""
+  ) {
     return;
   }
 
@@ -22,7 +43,8 @@ const addMovieHandler = () => {
   };
 
   movies.push(newMovie);
-  console.log(newMovie);
+  console.log(movies);
+  renderMovies();
 };
 
 addMovieBtn.addEventListener("click", addMovieHandler);
