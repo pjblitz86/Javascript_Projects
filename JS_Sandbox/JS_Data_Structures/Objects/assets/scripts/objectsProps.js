e5916ac1ce3d4e55d878c874a5018a1d70f39abf
Paulius_Juknis <paulius.juknis@gmail.com>
2019-12-11 10:02:59 +0200

    object spread operator

--- JS_Sandbox/JS_Data_Structures/Objects/assets/scripts/objectsProps.js
+++ JS_Sandbox/JS_Data_Structures/Objects/assets/scripts/objectsProps.js
@@ -30,3 +30,17 @@ delete person.hobbies; // good way
 person.age = undefined; // not clean
 
 console.log(person);
+
+// object spread operator
+const person2 = { name: "Paul", hobbies: ["chess", "programming"] };
+
+const person3 = person2;
+person2.age = "33";
+console.log(person3); // will have same props, cause it's referenced
+
+// but with object spread operator it creates a copy not a reference
+const anotherPerson = { ...person2 };
+person2.city = "Kaunas";
+console.log(anotherPerson); // wont add the property cause it was a copied object
+person2.hobbies.push("Sports");
+console.log(anotherPerson); // but array inside stayed referenced, cause it was not a deep nested object clone, just the top level.
