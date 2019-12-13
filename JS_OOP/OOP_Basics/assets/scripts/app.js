6c95e44bec3222fad6cf6a8181d69e17a2365091
Paulius_Juknis <paulius.juknis@gmail.com>
2019-12-13 15:39:56 +0200

    refactoring code to OOP with classes

--- JS_OOP/OOP_Basics/assets/scripts/app.js
+++ JS_OOP/OOP_Basics/assets/scripts/app.js
@@ -1,40 +1,64 @@
-const productList = {
-  products: [
-    {
-      title: "A Pillow",
-      imageUrl: "pillow",
-      price: 9.99,
-      description: "A soft pillow"
-    },
-    {
-      title: "A Carpet",
-      imageUrl: "carpet",
-      price: 89.99,
-      description: "a carpet for ya"
-    }
-  ],
+class Product {
+  title = "default"; // fields - "properties" of class
+  imageUrl;
+  description;
+  price;
+
+  constructor(title, image, desc, price) {
+    this.title = title;
+    this.imageUrl = image;
+    this.description = desc;
+    this.price = price;
+  }
+}
+
+class ProductList {
+  products = [
+    new Product("A Pillow", "pillow", "soft pillow", 9.99), // now thats the way of code reuse on objects, we create instance of class
+    new Product("A Carpet", "carpet", "a carpet for ya", 89.99)
+
+    // {
+    //   title: "A Carpet",
+    //   imageUrl: "carpet",
+    //   price: 89.99,
+    //   description: "a carpet for ya"
+    // } // hard to write reusable code with this structure, always have add manually, solution: use classes
+  ];
   render() {
     const renderHook = document.getElementById("app");
     const prodList = document.createElement("ul");
     prodList.className = "product-list";
     for (const prod of this.products) {
-      const prodEl = document.createElement("li");
-      prodEl.classList = "product-item";
-      prodEl.innerHTML = `
-        <div>
-          <img src="${prod.imageUrl}" alt="${prod.title}"/> 
-          <div class="product-item__content">
-            <h2>${prod.title}</h2>
-            <h3>${prod.price}</h3>
-            <p>${prod.description}</p>
-            <button>Add to cart</button>
-          </div>
-        </div>
-      `;
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
       prodList.append(prodEl);
     }
     renderHook.append(prodList);
   }
-};
-// hard to write reusable code with this structure, always have add manually, solution: use classes
+}
+
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  render() {
+    const prodEl = document.createElement("li");
+    prodEl.classList = "product-item";
+    prodEl.innerHTML = `
+      <div>
+        <img src="${this.product.imageUrl}" alt="${this.product.title}"/> 
+        <div class="product-item__content">
+          <h2>${this.product.title}</h2>
+          <h3>${this.product.price}</h3>
+          <p>${this.product.description}</p>
+          <button>Add to cart</button>
+        </div>
+      </div>
+    `;
+    return prodEl;
+  }
+}
+
+const productList = new ProductList();
 productList.render();
