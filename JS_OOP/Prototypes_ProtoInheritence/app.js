98ee0bbe88a2695ca013822330376c7e6666ce92
Paulius_Juknis <paulius.juknis@gmail.com>
2019-12-30 14:51:04 +0200

    understanding prototypes

--- JS_OOP/Prototypes_ProtoInheritence/app.js
+++ JS_OOP/Prototypes_ProtoInheritence/app.js
@@ -24,5 +24,19 @@ function Person() {
   };
 }
 
+// Prototypes - like a base class from which everyone inherits implicitly
+// add a method to prototype
+Person.prototype = {
+  printAge() {
+    console.log(this.age);
+  }
+};
+
 const person1 = new Person(); // behind the scenes new object in constructor function is created, properties added and object returned
 person1.greet();
+
+console.log(person1);
+console.log(person1.toString()); // [object Object] works cause prototype has this method in prototype chain
+
+person1.printAge(); // our added method, it's not found in person1 instance directly, but in prototype chain, so gets called.
+console.log(person1.__proto__);
