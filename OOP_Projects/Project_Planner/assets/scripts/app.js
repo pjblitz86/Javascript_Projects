9c1fb4be9a9fc2d2e5581169aba0b75f3d314cad
Paulius_Juknis <paulius.juknis@gmail.com>
2019-12-31 10:39:21 +0200

    switching between project lists logic

--- OOP_Projects/Project_Planner/assets/scripts/app.js
+++ OOP_Projects/Project_Planner/assets/scripts/app.js
@@ -1,17 +1,60 @@
 class Tooltip {}
 
-class ProjectItem {}
+class ProjectItem {
+  constructor(id, updateProjectListsFunction) {
+    this.updateProjectListsHandler = updateProjectListsFunction;
+    this.id = id;
+    this.connectSwitchButton();
+    this.connectMoreInfoButton();
+  }
+
+  connectMoreInfoButton() {}
+
+  connectSwitchButton() {
+    const projectItemElement = document.getElementById(this.id);
+    const switchBtn = projectItemElement.querySelector("button:last-of-type");
+    switchBtn.addEventListener("click", this.updateProjectListsHandler);
+  }
+}
 
 class ProjectList {
+  projects = [];
+
   constructor(type) {
+    this.type = type;
     const prjItems = document.querySelectorAll(`#${type}-projects li`);
+    for (const prjItem of prjItems) {
+      this.projects.push(
+        new ProjectItem(prjItem.id, this.switchProject.bind(this))
+      );
+    }
+    console.log(this.projects);
+  }
+
+  setSwitchHandlerFunction(switchHandlerFunction) {
+    this.switchHandler = switchHandlerFunction;
+  }
+
+  addProject() {
+    console.log(this);
+  }
+
+  switchProject(projectId) {
+    this.switchHandler(this.projects.find(p => p.id === projectId));
+    this.projects = this.projects.filter(p => p.id !== projectId);
   }
 }
 
 class App {
   static init() {
-    const activeProject = new ProjectList("active");
-    const finishedProject = new ProjectList("finished");
+    const activeProjectList = new ProjectList("active");
+    const finishedProjectList = new ProjectList("finished");
+    activeProjectList.setSwitchHandlerFunction(
+      finishedProjectList.addProject.bind(finishedProjectList)
+    );
+    finishedProjectList.setSwitchHandlerFunction(
+      activeProjectList.addProject.bind(activeProjectList)
+    );
   }
 }
 
