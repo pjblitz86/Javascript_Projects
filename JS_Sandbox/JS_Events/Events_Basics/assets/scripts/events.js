2cb68bb026e0dca7139d07d49680b59e5d7d0ee8
Paulius_Juknis <paulius.juknis@gmail.com>
2020-01-02 15:31:02 +0200

    playaround with events

--- JS_Sandbox/JS_Events/Events_Basics/assets/scripts/events.js
+++ JS_Sandbox/JS_Events/Events_Basics/assets/scripts/events.js
@@ -2,13 +2,26 @@ const button = document.querySelector("button");
 
 // button.onclick = function() {} // older way
 
-const buttonClickHandler = () => {
+const buttonClickHandler = event => {
   alert("Button was clicked...");
+  event.target.disabled = true;
+  console.log(event);
 };
 
-const anotherButtonClickHandler = () => {
-  console.log("This was clicked");
-};
+// const anotherButtonClickHandler = () => {
+//   console.log("This was clicked");
+// };
 
-button.onclick = buttonClickHandler; // downside - can only attach 1 handler
-button.onclick = anotherButtonClickHandler; // wont work on 2, overrides - gets the last event attached only
+// button.onclick = buttonClickHandler; // downside - can only attach 1 handler
+// button.onclick = anotherButtonClickHandler; // wont work on 2, overrides - gets the last event attached only
+
+button.addEventListener("click", buttonClickHandler); // allows multiple event listeners to the same element
+
+// setTimeout(() => {
+//   button.removeEventListener("click", buttonClickHandler); // same as addeventlistener
+// }, 2000);
+
+// scroll events - careful for not slowing down the page
+// window.addEventListener("scroll", event => {
+//   console.log(event);
+// });
