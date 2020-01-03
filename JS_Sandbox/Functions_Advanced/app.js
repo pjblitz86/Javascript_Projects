3daf4a22a2f560257a3b638d8b82715483c5982b
Paulius_Juknis <paulius.juknis@gmail.com>
2020-01-03 16:10:54 +0200

    recursion example

--- JS_Sandbox/Functions_Advanced/app.js
+++ JS_Sandbox/Functions_Advanced/app.js
@@ -30,13 +30,14 @@ function createTaxCalculator(tax) {
   return calculateTax;
 }
 
-// for this scenario factory function is helpful, cause 1 param changes always
+// for this scenario factory function is helpful, cause 1 param changes always. also good closure example
 const calculateVATAmount = createTaxCalculator(0.19);
 const calculateIncomeTaxAmount = createTaxCalculator(0.25);
 
 // Closures
 // every function in JS is a closure, cause it's scoped and registers surrounding lexical scoped environment if it has used variables
 // Closures are basically the walking dead. When a function returns, the idea is that all of the variables within that function should be non existent. Yet, closures still have access to any variables in their scope chains.
+// Closures are functions with preserved data
 
 multiplier = 1.2;
 console.log(calculateVATAmount(100));
@@ -53,4 +54,44 @@ let name = "Peter";
 
 userName = "Paul"; // closure uses the latest value in their lexical scope
 
+console.dir(greetUser);
 greetUser();
+
+// IIFE  - immediately invoked function expression, old school pattern
+
+(function() {
+  var age = 30;
+  console.log(age); // 30
+})();
+
+// with let and const we don't really need it anymore
+{
+  const age = 30;
+  console.log(age); // 30
+}
+
+// RECURSION
+
+// without recursion
+// function powerOf(x, n) {
+//   let result = 1;
+
+//   for (let i = 0; i < n; i++) {
+//     result *= x;
+//   }
+//   return `Result: ${result}`;
+// }
+
+// with recursion - function calls itself and is placed in the stack, when return happens it is executed and removed from call stack with saved value
+
+function powerOf(x, n) {
+  // if (n === 1) {
+  //   // otherwise infinite loop
+  //   return x;
+  // }
+  // return x * powerOf(x, n - 1);
+
+  return n === 1 ? x : x * powerOf(x, n - 1); // shortest possible
+}
+
+console.log(powerOf(2, 3));
