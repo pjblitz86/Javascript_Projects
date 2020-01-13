19e4864c44af1a2ae242c2469dabeeff3cd371cf
Paulius_Juknis <paulius.juknis@gmail.com>
2020-01-13 14:00:47 +0200

    connect requests to UI

--- JS_Sandbox_AJAX/SendingHttpRequests/Http_Basics/assets/scripts/app.js
+++ JS_Sandbox_AJAX/SendingHttpRequests/Http_Basics/assets/scripts/app.js
@@ -1,7 +1,9 @@
-// XMLHttp Request - old school way - not done this way anymore, just for info, use fetchAPI or axios instead.
+// XMLHttp Request - old school way - just for understanding, use fetchAPI or axios instead.
 
 const listElement = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
+const form = document.querySelector("#new-post");
+const fetchBtn = document.querySelector("#available-posts button");
 
 function sendHttpRequest(method, url, data) {
   const promise = new Promise((resolve, reject) => {
@@ -43,5 +45,11 @@ async function createPost(title, content) {
   sendHttpRequest("POST", "https://jsonplaceholder.typicode.com/posts", post);
 }
 
-getPosts();
-createPost("Test", "A test post");
+fetchBtn.addEventListener("click", getPosts);
+form.addEventListener("submit", event => {
+  event.preventDefault();
+  // validate
+  const enteredTitle = event.currentTarget.querySelector("#title").value;
+  const enteredContent = event.currentTarget.querySelector("#content").value;
+  createPost(enteredTitle, enteredContent);
+});
