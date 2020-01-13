dc9c72936372a0f592cfae6cc8e99f10d4b6af1f
Paulius_Juknis <paulius.juknis@gmail.com>
2020-01-13 13:48:54 +0200

    send post request

--- JS_Sandbox_AJAX/SendingHttpRequests/Http_Basics/assets/scripts/app.js
+++ JS_Sandbox_AJAX/SendingHttpRequests/Http_Basics/assets/scripts/app.js
@@ -1,23 +1,24 @@
 // XMLHttp Request - old school way - not done this way anymore, just for info, use fetchAPI or axios instead.
 
-const xhr = new XMLHttpRequest();
 const listElement = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
 
-function sendHttpRequest(method, url) {
+function sendHttpRequest(method, url, data) {
   const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
     xhr.open(method, url);
     xhr.responseType = "json"; // we dont need JSON.parse then
 
     xhr.onload = function() {
       resolve(xhr.response);
     };
-    xhr.send();
+    xhr.send(JSON.stringify(data));
   });
   return promise;
 }
 
-function fetchPosts() {
+// using then chaining
+function getPosts() {
   sendHttpRequest("GET", "https://jsonplaceholder.typicode.com/posts").then(
     responseData => {
       const listOfPosts = responseData;
@@ -31,4 +32,16 @@ function fetchPosts() {
   );
 }
 
-fetchPosts();
+// in async/await
+async function createPost(title, content) {
+  const userId = Math.random();
+  const post = {
+    title: title,
+    body: content,
+    userId: userId
+  };
+  sendHttpRequest("POST", "https://jsonplaceholder.typicode.com/posts", post);
+}
+
+getPosts();
+createPost("Test", "A test post");
