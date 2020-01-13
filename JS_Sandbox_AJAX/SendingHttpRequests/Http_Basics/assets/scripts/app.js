d9fb93087c2e83e13f7cd78f1f52ddcbefbcd134
Paulius_Juknis <paulius.juknis@gmail.com>
2020-01-13 13:29:28 +0200

    refactor into reusable function

--- JS_Sandbox_AJAX/SendingHttpRequests/Http_Basics/assets/scripts/app.js
+++ JS_Sandbox_AJAX/SendingHttpRequests/Http_Basics/assets/scripts/app.js
@@ -4,18 +4,31 @@ const xhr = new XMLHttpRequest();
 const listElement = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
 
-xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
-xhr.responseType = "json"; // we dont need JSON.parse then
+function sendHttpRequest(method, url) {
+  const promise = new Promise((resolve, reject) => {
+    xhr.open(method, url);
+    xhr.responseType = "json"; // we dont need JSON.parse then
 
-xhr.onload = function() {
-  // const listOfPosts = JSON.parse(xhr.response);
-  const listOfPosts = xhr.response;
-  for (const post of listOfPosts) {
-    const postEl = document.importNode(postTemplate.content, true);
-    postEl.querySelector("h2").textContent = post.title.toUpperCase();
-    postEl.querySelector("p").textContent = post.body;
-    listElement.append(postEl);
-  }
-};
+    xhr.onload = function() {
+      resolve(xhr.response);
+    };
+    xhr.send();
+  });
+  return promise;
+}
 
-xhr.send();
+function fetchPosts() {
+  sendHttpRequest("GET", "https://jsonplaceholder.typicode.com/posts").then(
+    responseData => {
+      const listOfPosts = responseData;
+      for (const post of listOfPosts) {
+        const postEl = document.importNode(postTemplate.content, true);
+        postEl.querySelector("h2").textContent = post.title.toUpperCase();
+        postEl.querySelector("p").textContent = post.body;
+        listElement.append(postEl);
+      }
+    }
+  );
+}
+
+fetchPosts();
