ce8269f093052848e3f7556692cc41c84cf23917
Paulius_Juknis <paulius.juknis@gmail.com>
2020-01-17 10:50:23 +0200

    implement event listener delegation and  send delete request

--- JS_Sandbox_AJAX/SendingHttpRequests/Http_Basics/assets/scripts/app.js
+++ JS_Sandbox_AJAX/SendingHttpRequests/Http_Basics/assets/scripts/app.js
@@ -4,6 +4,7 @@ const listElement = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
 const form = document.querySelector("#new-post");
 const fetchBtn = document.querySelector("#available-posts button");
+const postList = document.querySelector("ul"); // dont add event listener on all li, but use delegation
 
 function sendHttpRequest(method, url, data) {
   const promise = new Promise((resolve, reject) => {
@@ -28,6 +29,7 @@ function getPosts() {
         const postEl = document.importNode(postTemplate.content, true);
         postEl.querySelector("h2").textContent = post.title.toUpperCase();
         postEl.querySelector("p").textContent = post.body;
+        postEl.querySelector("li").id = post.id;
         listElement.append(postEl);
       }
     }
@@ -52,4 +54,14 @@ form.addEventListener("submit", event => {
   const enteredTitle = event.currentTarget.querySelector("#title").value;
   const enteredContent = event.currentTarget.querySelector("#content").value;
   createPost(enteredTitle, enteredContent);
+});
+
+postList.addEventListener("click", event => {
+  if (event.target.tagName === "BUTTON") {
+    const postId = event.target.closest("li").id;
+    sendHttpRequest(
+      "DELETE",
+      `https://jsonplaceholder.typicode.com/posts/${postId}`
+    );
+  }
 });
