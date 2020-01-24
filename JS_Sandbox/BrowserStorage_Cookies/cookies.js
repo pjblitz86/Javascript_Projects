11d07f71bf33f8f20d591f5e54b7578ebccdd9d6
Paulius_Juknis <paulius.juknis@gmail.com>
2020-01-24 13:46:34 +0200

    cookies starting files

---
+++ JS_Sandbox/BrowserStorage_Cookies/cookies.js
@@ -0,0 +1,8 @@
+console.log(document.cookie);
+
+const storeBtn = document.getElementById("store-btn");
+const retrieveBtn = document.getElementById("retrieve-btn");
+
+storeBtn.addEventListener("click", () => {});
+
+retrieveBtn.addEventListener("click", () => {});
