d87cb73fcf9df62757e84ad49133030613e0f07a
Paulius_Juknis <paulius.juknis@gmail.com>
2020-01-24 13:59:32 +0200

    set cookie

--- JS_Sandbox/BrowserStorage_Cookies/cookies.js
+++ JS_Sandbox/BrowserStorage_Cookies/cookies.js
@@ -3,6 +3,11 @@ console.log(document.cookie);
 const storeBtn = document.getElementById("store-btn");
 const retrieveBtn = document.getElementById("retrieve-btn");
 
-storeBtn.addEventListener("click", () => {});
+storeBtn.addEventListener("click", () => {
+  const userId = "u123";
+  document.cookie = `uid=${userId}`; // adds a cookie
+});
 
-retrieveBtn.addEventListener("click", () => {});
+retrieveBtn.addEventListener("click", () => {
+  console.log(document.cookie);
+});
