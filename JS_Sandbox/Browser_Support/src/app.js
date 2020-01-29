920e75d63be20853b16160782b21395535d62afb
Paulius_Juknis <paulius.juknis@gmail.com>
2020-01-29 09:44:23 +0200

    browser support example - clibboard api

--- JS_Sandbox/Browser_Support/src/app.js
+++ JS_Sandbox/Browser_Support/src/app.js
@@ -1,5 +1,22 @@
 const button = document.querySelector("button");
+const textParagraph = document.querySelector("p");
+
+// clipboard api - would work on chrome, but not edge, safari for example
 
 button.addEventListener("click", () => {
-  // do something...
+  const text = textParagraph.textContent;
+  if (navigator.clipboard) {
+    // wont run if browser doesnt support it
+    navigator.clipboard
+      .writeText(text)
+      .then(result => {
+        console.log(result);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  } else {
+    // fallback
+    alert("Feauture not available");
+  }
 });
