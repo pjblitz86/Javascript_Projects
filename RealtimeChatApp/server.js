77e6270455a78c88b4163fdb4cf5dc61cae33eec
Paulius_Juknis <paulius.juknis@gmail.com>
2020-04-16 12:58:00 +0300

    setup socketio

--- RealtimeChatApp/server.js
+++ RealtimeChatApp/server.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const http = require("http");
+const path = require("path");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => {
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+  console.log("New WS connection..");
+});
+
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
