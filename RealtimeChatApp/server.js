f9c8ecf62e6dabb93bacd9e087f47210c91f4308
Paulius_Juknis <paulius.juknis@gmail.com>
2020-04-16 15:05:54 +0300

    changing hardcoded values with dynamic

--- RealtimeChatApp/server.js
+++ RealtimeChatApp/server.js
@@ -2,25 +2,31 @@ const express = require("express");
 const http = require("http");
 const path = require("path");
 const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
 const PORT = process.env.PORT || 3000;
+const botName = "ChatBot";
 
 app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", (socket) => {
-  socket.emit("message", "Welcome to PJ Chat");
+  socket.emit("message", formatMessage(botName, "Welcome to PJ Chat"));
 
-  socket.broadcast.emit("message", "A user has joined the chat");
+  socket.broadcast.emit(
+    "message",
+    formatMessage(botName, "A user has joined the chat")
+  );
 
   socket.on("disconnect", () => {
-    io.emit("message", "A user has left the chat");
+    io.emit("message", formatMessage(botName, "A user has left the chat"));
   });
 
   socket.on("chatMessage", (msg) => {
-    io.emit("message", msg);
+    io.emit("message", formatMessage("USER", msg));
   });
 });
 
