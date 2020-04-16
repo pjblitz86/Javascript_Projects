2567ad0b91e27149f8cf1a63e8bb4a490e9b2f5c
Paulius_Juknis <paulius.juknis@gmail.com>
2020-04-16 14:16:00 +0300

    scroll down with messages, clear input and focus after sending

--- RealtimeChatApp/public/js/main.js
+++ RealtimeChatApp/public/js/main.js
@@ -1,17 +1,19 @@
 const socket = io();
 const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
 
 socket.on("message", (message) => {
   console.log(message);
   outputMessage(message);
+  chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 chatForm.addEventListener("submit", (e) => {
   e.preventDefault();
-
   const msg = e.target.elements.msg.value;
-
   socket.emit("chatMessage", msg);
+  e.target.elements.msg.value = "";
+  e.target.elements.msg.focus();
 });
 
 function outputMessage(message) {
--- RealtimeChatApp/server.js
+++ RealtimeChatApp/server.js
@@ -6,7 +6,7 @@ const socketio = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
