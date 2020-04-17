a7352df3ab1131e5d372f2e7640e2bb8e0ca1cc2
Paulius_Juknis <paulius.juknis@gmail.com>
2020-04-17 14:02:44 +0300

    adding some components and scss for them

---
+++ JS_Workflow_no_framework/src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+  const template = `
+  <header>
+    <h1>Ma Parcel App</h1>
+    <p>This is a boilderplate for a simple JS workflow using parcel</p>
+  </header>
+  `;
+  return template;
+};
+
+export default Header;
