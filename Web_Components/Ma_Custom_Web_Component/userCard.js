57d66d423cbc0eb2143ddd51a5cd19c135a28fbb
Paulius_Juknis <paulius.juknis@gmail.com>
2020-04-20 13:35:43 +0300

    template and shadow dom for web component

--- Web_Components/Ma_Custom_Web_Component/userCard.js
+++ Web_Components/Ma_Custom_Web_Component/userCard.js
@@ -1,7 +1,26 @@
+// Need to create template so global/outside css doesnt affect this web component and use shadow DOM
+
+const template = document.createElement("template");
+template.innerHTML = `
+  <style>
+    h3 {
+      color: blue;
+    }
+  </style>
+  <div class="user-card">
+    <h3></h3>
+  </div>
+
+`;
+
 class UserCard extends HTMLElement {
   constructor() {
     super();
-    this.innerHTML = `Paul Jukman`;
+    // shadow DOM
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+    this.shadowRoot.querySelector("h3").innerText = this.getAttribute("name");
   }
 }
 
