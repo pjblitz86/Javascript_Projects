bdb4b7e790fc8a992c40ef2105b6a959c36b7039
Paulius_Juknis <paulius.juknis@gmail.com>
2020-04-20 14:19:43 +0300

    implement show info in shadow dom

--- Web_Components/Ma_Custom_Web_Component/userCard.js
+++ Web_Components/Ma_Custom_Web_Component/userCard.js
@@ -44,12 +44,41 @@ template.innerHTML = `
 class UserCard extends HTMLElement {
   constructor() {
     super();
+
+    this.showInfo = true;
+
     // shadow DOM
     this.attachShadow({ mode: "open" });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
 
     this.shadowRoot.querySelector("h3").innerText = this.getAttribute("name");
     this.shadowRoot.querySelector("img").src = this.getAttribute("avatar");
+  }
+
+  toggleInfo() {
+    this.showInfo = !this.showInfo;
+    const info = this.shadowRoot.querySelector(".info");
+    const toggleBtn = this.shadowRoot.querySelector("#toggle-info");
+
+    if (this.showInfo) {
+      info.style.display = "block";
+      toggleBtn.innerHTML = "Hide Info";
+    } else {
+      info.style.display = "none";
+      toggleBtn.innerHTML = "Show Info";
+    }
+  }
+
+  connectedCallback() {
+    this.shadowRoot
+      .querySelector("#toggle-info")
+      .addEventListener("click", () => {
+        this.toggleInfo();
+      });
+  }
+
+  disconnectedCallback() {
+    this.shadowRoot.querySelector("#toggle-info").removeEventListener();
   }
 }
 
